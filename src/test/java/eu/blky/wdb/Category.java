aaeddc60b5e2203af3d26b200ef81d90de0ee0f0
vasiliij pupkin <vasiliij.pupkin@gmail.com>
2011-12-20 16:06:11 +0100

    category/Test added to WDB Shelf={ 	book== 	[Book:[prose]={ 		Author== 		[Author={ 			Second name== 			[Miguel 			], 			First name== 			[Cervantes 			]} 		], 		Title== 		[Don Quijote 		]} 	]]; 	[Book:[book]={ 		Author== 		[Author={ 			Second name== 			[Miguel 			], 			First name== 			[Cervantes 			]} 		], 		Title== 		[Kornelia 		]} 	], 	Color== 	[red 	]}

--- src/test/java/eu/blky/wdb/Category.java
+++ src/test/java/eu/blky/wdb/Category.java
@@ -1,5 +1,6 @@
 package eu.blky.wdb;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /** 
@@ -11,16 +12,30 @@ import java.util.Set;
  * 
  * Creation:  19.12.2011::21:35:16<br> 
  */
-public class Category extends Wdb{
+public class Category extends Wdb{ 
 
-	
+	/**
+	 * @author vipup
+	 */
+	private static final long serialVersionUID = 6581281116649476813L;
+	private Category parent;
+
 
 	public Category(String oName) {
 		super(oName);  
 	}
 
+	public Category(Category parentCategory, String name) { 
+		super(name); 
+		if (this.parent==null){
+			this.parent=parentCategory; 
+		}else{
+			this.parent.add(parentCategory);
+		}
+	}
+
 	// n-to-m relation
-	private Set<Wdb> instances;
+	private Set<Wdb> instances = new HashSet<Wdb>();
 	
 	
 	public void addInstance(Wdb wdb) {
