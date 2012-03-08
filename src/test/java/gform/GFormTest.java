f5ad604b3c02498817a183ee3b2d7daecd6696af
vasiliij pupkin <vasiliij.pupkin@gmail.com>
2012-03-08 10:38:46 +0100

    JUnit v.4.x.x -> 3.x.x

--- src/test/java/gform/GFormTest.java
+++ src/test/java/gform/GFormTest.java
@@ -1,31 +1,15 @@
 package gform;
-
-import static org.junit.Assert.*;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.ArrayList;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPathExpressionException;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.xml.sax.SAXException;
+ 
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.IOException; 
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
-
+ 
 import org.xml.sax.SAXException;
+ 
 
 import junit.framework.TestCase;
 
@@ -42,29 +26,25 @@ public class GFormTest extends TestCase {
 
 	/**
 	 * @throws java.lang.Exception
-	 */
-	@BeforeClass
+	 */ 
 	public static void setUpBeforeClass() throws Exception {
 	}
 
 	/**
 	 * @throws java.lang.Exception
-	 */
-	@AfterClass
+	 */ 
 	public static void tearDownAfterClass() throws Exception {
 	}
 
 	/**
 	 * @throws java.lang.Exception
-	 */
-	@Before
+	 */ 
 	public void setUp() throws Exception {
 	}
 
 	/**
 	 * @throws java.lang.Exception
-	 */
-	@After
+	 */ 
 	public void tearDown() throws Exception {
 	}
 
@@ -75,8 +55,7 @@ public class GFormTest extends TestCase {
 	 * @throws ParserConfigurationException 
 	 * @throws XPathExpressionException 
 	 * @throws FileNotFoundException 
-	 */
-	@Test
+	 */ 
 	public void testParceTableName() throws FileNotFoundException, XPathExpressionException, ParserConfigurationException, SAXException, IOException {
 		System.out.println("parceTableName");
 		
