35ba066ebb12794379643d3b024d5598f8b6c77b
vasiliij pupkin <vasiliij.pupkin@gmail.com>
2012-05-22 14:03:49 +0200

    http://localhost:8080/GForm/addressSuggestion.jsp ++ @see http://ramakrsna.wordpress.com/2008/03/16/ajax-address-suggestion-using-lucene-and-j2ee/

---
+++ src/main/java/com/dataServices/GetAddress.java
@@ -0,0 +1,72 @@
+package com.dataServices;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.File;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dataConversion.Obj2Xml;
+import com.objects.Properties;
+
+/**
+ * Servlet implementation class for Servlet: GetAddress
+ *
+ */
+ public class GetAddress extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
+   static final long serialVersionUID = 1L;
+   
+    /* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#HttpServlet()
+	 */
+	public GetAddress() {
+		super();
+	}   	
+	
+	/* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}  	
+	
+	/* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/xml");
+		response.setHeader("Cache-Control", "no-cache");
+		Properties properties = new Properties();
+		String queryString = request.getParameter("searchString").trim();
+		queryString = queryString.replaceAll(" ", "*");
+		System.out.println(queryString);
+		Obj2Xml obj2Xml = new Obj2Xml();
+		PrintWriter pw = response.getWriter();
+		try {
+		AddressSearch addressSearch = AddressSearch.getAddressSearch();
+		properties.setProperty(addressSearch.search("*" + queryString + "*"));
+		obj2Xml.persistAddressList(properties, pw);
+		}
+		catch(Exception ex) {
+			getServletContext().log(ex, "Exception");
+		}
+		pw.close();
+	}   	  	    
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		//get the absolute path of the xml file
+		String xmlFilePath = getServletConfig().getInitParameter("dataStore") + File.pathSeparator + "nrhp.xml";
+		//Initiate data of address data to memory
+		try {
+			AddressSearch.initialize(xmlFilePath);
+		} catch (Exception e) {
+			getServletContext().log(e, "Exception");
+			e.printStackTrace();
+		}
+	}
+}
