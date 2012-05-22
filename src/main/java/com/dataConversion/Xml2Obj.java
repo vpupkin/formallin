7ed4a74de05c0317420abd603cfd18aecde2f0d7
vasiliij pupkin <vasiliij.pupkin@gmail.com>
2012-05-22 14:59:25 +0200

    http://localhost:8080/GForm/addressSuggestion.jsp ++ RESOURSE++

--- src/main/java/com/dataConversion/Xml2Obj.java
+++ src/main/java/com/dataConversion/Xml2Obj.java
@@ -1,6 +1,7 @@
 package com.dataConversion;
 
-import java.io.File;
+import java.io.File;  
+import java.io.InputStream;
 import java.util.List;
 
 import javax.xml.bind.JAXBContext;
@@ -11,8 +12,13 @@ public class Xml2Obj {
 	public List<com.objects.Property> getAddressList(String xmlFilePath) throws JAXBException {
 		JAXBContext jc = JAXBContext.newInstance("com.objects");
 		Unmarshaller unmarshaller = jc.createUnmarshaller();
-		com.objects.Properties properties =
-			(com.objects.Properties)unmarshaller.unmarshal(new File(xmlFilePath));
+		com.objects.Properties properties = null;
+		try{
+			properties = (com.objects.Properties)unmarshaller.unmarshal(new File(xmlFilePath));
+		}catch(Exception e){
+			InputStream classpathXMLReasource = this.getClass().getClassLoader().getResourceAsStream("com/dataConversion/nrhp.xml");
+			properties = (com.objects.Properties)unmarshaller.unmarshal(classpathXMLReasource);
+		}
 		return properties.getProperty();
 	}
 }
